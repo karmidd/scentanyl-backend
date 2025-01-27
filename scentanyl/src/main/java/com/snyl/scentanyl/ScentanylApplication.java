91b0a61d31afe1f71aedd6383654c1ffb62608b3
Karam Midani <karambassmid@hotmail.com>
2025-01-26 23:15:01 -0500

    Added Spring Boot project files

---
+++ scentanyl/src/main/java/com/snyl/scentanyl/ScentanylApplication.java
@@ -0,0 +1,13 @@
+package com.snyl.scentanyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScentanylApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScentanylApplication.class, args);
+	}
+
+}
---
+++ scentanyl/src/test/java/com/snyl/scentanyl/ScentanylApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snyl.scentanyl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScentanylApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
