331196d18dd5cdb43a675c4c7d1ab9741206ecae
Karam Midani <karambassmid@hotmail.com>
2025-02-12 18:16:50 -0500

    Added FragranceRepository headers and their respective methods in FragranceService. Refactored instances in Fragrance class with camel case. Added DevTools to pom.xml.

--- scentanyl/src/main/java/com/snyl/scentanyl/fragrance/FragranceRepository.java
+++ scentanyl/src/main/java/com/snyl/scentanyl/fragrance/FragranceRepository.java
@@ -3,10 +3,27 @@ package com.snyl.scentanyl.fragrance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FragranceRepository extends JpaRepository<Fragrance, String> {
     void deleteByUrl(String url);
     Optional<Fragrance> findByUrl(String url);
+    Optional<Fragrance> findByBrand(String brand);
+    List<Fragrance> findAllByFragranceName(String fragranceName);
+    List<Fragrance> findAllByBrand(String brand);
+    List<Fragrance> findAllByGender(String gender);
+    List<Fragrance> findAllByPerfumer1(String perfumer1);
+    List<Fragrance> findAllByPerfumer2(String perfumer2);
+    List<Fragrance> findAllByTopNotes(String topNotes);
+    List<Fragrance> findAllByMiddleNotes(String middleNotes);
+    List<Fragrance> findAllByBaseNotes(String baseNotes);
+    List<Fragrance> findAllByMainaccord1(String mainaccord1);
+    List<Fragrance> findAllByMainaccord2(String mainaccord2);
+    List<Fragrance> findAllByMainaccord3(String mainaccord3);
+    List<Fragrance> findAllByMainaccord4(String mainaccord4);
+    List<Fragrance> findAllByMainaccord5(String mainaccord5);
+    List<Fragrance> findAllByCountry(String country);
+    List<Fragrance> findAllByYear(Integer year);
 }
