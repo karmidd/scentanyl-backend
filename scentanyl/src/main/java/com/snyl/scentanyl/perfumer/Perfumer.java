d49367bb4f411ebdf5fef0db0b8b1a6d5aa72936
Karam Midani <karambassmid@hotmail.com>
2025-08-21 01:21:25 -0400

    JsonIgnore on unneeded (for now) column

--- scentanyl/src/main/java/com/snyl/scentanyl/perfumer/Perfumer.java
+++ scentanyl/src/main/java/com/snyl/scentanyl/perfumer/Perfumer.java
@@ -1,5 +1,6 @@
 package com.snyl.scentanyl.perfumer;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,6 +20,7 @@ public class Perfumer {
 
     private String name;
 
+    @JsonIgnore
     private String image;
 
     @Column(name = "total_fragrances")
